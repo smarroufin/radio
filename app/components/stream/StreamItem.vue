d6a2eb19eefbb63afac7f901ee530f78c1112608
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2025-02-14 18:14:55 +0100

    chore(stream): add tags

--- app/components/stream/StreamItem.vue
+++ app/components/stream/StreamItem.vue
@@ -1,37 +1,42 @@
 <script setup lang="ts">
-defineProps({
+const props = defineProps({
   stream: {
     type: Object as PropType<RadioBrowserStream>,
     required: true,
   },
 })
+
+const tags = computed(() => {
+  return props.stream.tags.split(',').map(tag => tag.trim()).filter(tag => !!tag)
+})
 </script>
 
 <template>
   <div class="flex items-center gap-4 py-1">
-    <div class="flex-1 min-w-0 flex items-center gap-2">
-      <span class="flex-shrink font-medium truncate">{{ stream.name }}</span>
-      <AppCountryFlag
-        :country="stream.country"
-        :country-code="stream.countrycode"
-      />
-      <UButton
-        icon="i-carbon-launch"
-        variant="link"
-        size="sm"
-        :to="stream.homepage"
-        target="_blank"
-        aria-label="Open stream homepage"
-        class="p-0"
-      />
-      <!-- TODO: use votes & tags -->
-      <!-- <span>{{ stream.votes }}</span>
-      <UTooltip
-        :text="stream.tags"
-        class="hidden lg:flex"
-      >
-        <span>Tags</span>
-      </UTooltip> -->
+    <div class="flex-1 min-w-0 flex flex-col gap-1">
+      <div class="flex items-center gap-2">
+        <span class="flex-shrink font-medium truncate">{{ stream.name }}</span>
+        <UButton
+          icon="i-carbon-launch"
+          variant="link"
+          size="sm"
+          :to="stream.homepage"
+          target="_blank"
+          aria-label="Open stream homepage"
+          class="p-0"
+        />
+      </div>
+      <div class="flex items-center gap-1 overflow-hidden">
+        <AppCountryFlag
+          :country="stream.country"
+          :country-code="stream.countrycode"
+        />
+        <span
+          v-for="tag of tags"
+          :key="tag"
+          class="bg-neutral-800 px-2 py-0.5 text-xs rounded-full whitespace-nowrap"
+        >{{ tag }}</span>
+      </div>
     </div>
     <div class="flex items-center gap-2">
       <StreamPinButton :stream="stream" />
