3eb7ec83932f27771e057b3556d70f3e327a6522
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2025-02-15 17:04:30 +0100

    chore(api): add user agent

--- server/api/streams/popular.get.ts
+++ server/api/streams/popular.get.ts
@@ -1,8 +1,13 @@
+import { version } from '../../../package.json'
+
 export default defineCachedEventHandler(async () => {
   const hosts = await cachedRadioBrowserHosts()
   const host = hosts[Math.floor(Math.random() * hosts.length)]
   return $fetch<RadioBrowserStream[]>('/stations', {
     baseURL: `${host}/json`,
+    headers: {
+      'User-Agent': `SmarroufinRadio/${version}`,
+    },
     query: {
       hidebroken: true,
       limit: 100,
