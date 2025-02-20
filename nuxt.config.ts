e3ec35236c7ce2d389aa69cf2ddf1cf5245ba6d4
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2025-02-20 17:40:42 +0100

    chore(deps): bump

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,9 +41,4 @@ export default defineNuxtConfig({
       stylistic: true,
     },
   },
-  icon: {
-    // Workaround: enabled fetch for circle-flags
-    // FIXME: only fetch for circle-flags collection
-    provider: 'iconify',
-  },
 })
